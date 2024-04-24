750eb265dbc2075439cd079e67851a0dff7e1cf1
ntua-el20827 <74141049+ntua-el20827@users.noreply.github.com>
2024-04-24 16:18:15 +0300

    Add files via upload

---
+++ 7_3/ex7_3_raytrace.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Define constant values
+L1c=128
+L1a=8
+L1b=128
+L2c=2048
+L2a=16
+L2b=256
+
+# Define tests configurations
+tests=(
+    "32 4 4096"
+    "32 8 4096"
+    "64 1 4096"
+    "64 2 4096"
+    "64 4 4096"
+    "64 8 4096"
+    "64 16 4096"
+    "64 32 4096"
+    "64 64 4096"
+    "128 4 4096"
+    "256 4 4096"
+)
+
+
+# Create a directory for storing output files
+output_dir="output_files/rtview_outputs"
+mkdir -p "$output_dir"
+
+# Run tests
+for ((i=0; i<${#tests[@]}; ++i)); do
+    test=${tests[$i]}
+    IFS=' ' read -r TLBe TLBa TLBp <<< "$test"
+
+    # Run command and store output in a file
+    output_file="$output_dir/output_$i.txt"
+    /home/george/Workshop/uni/8sem/archlab/ex1/pin-3.30-98830-g1d7b601b3-gcc-linux/pin -t /home/george/Workshop/uni/8sem/archlab/ex1/parsec-3.0/advcomparch-ex1-helpcode/pintool/obj-intel64/simulator.so -o "$output_file" -L1c "$L1c" -L1a "$L1a" -L1b "$L1b" -L2c "$L2c" -L2a "$L2a" -L2b "$L2b" -TLBe "$TLBe" -TLBa "$TLBa" -TLBp "$TLBp" -- /home/george/Workshop/uni/8sem/archlab/ex1/parsec-3.0/parsec_workspace/executables/rtview /home/george/Workshop/uni/8sem/archlab/ex1/parsec-3.0/parsec_workspace/inputs/happy_buddha.obj -automove -nthreads 1 -frames 3 -res 1920 1080
+
+    echo "Test $i completed. Output stored in $output_file"
+done
+
+# Find configuration with the highest IPC
+max_ipc=0
+max_ipc_config=""
+for file in "$output_dir"/*.txt; do
+    ipc=$(sed '6q;d' "$file" | awk '{print $2}')  # Adjusted to extract the value after "IPC:"
+    if (( $(echo "$ipc > $max_ipc" | bc -l) )); then
+        max_ipc=$ipc
+        max_ipc_config=$(basename "$file")
+    fi
+done
+
+# Print the values of the best configuration
+best_config_index=$(echo "$max_ipc_config" | cut -d '_' -f 2 | cut -d '.' -f 1)
+echo "Best configuration values: ${tests[$best_config_index]}"
+
+# Save the best configuration to a file
+echo "[RTVIEW] Best configuration: ${tests[$best_config_index]} with IPC $max_ipc" >> /home/george/Workshop/uni/8sem/archlab/ex1/ex7_3/best_configurations3.txt
+
+echo "[RESULT] Configuration with the highest IPC for RTVIEW is $max_ipc_config with IPC $max_ipc"
+
---
+++ 7_3/graph.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# Function to extract IPC value from output.txt file
+def extract_ipc(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith("IPC:"):
+                ipc_value = float(line.split(":")[1])
+                return ipc_value
+    return None
+
+# Directory containing the output folders
+output_folder_path = "/home/george/Workshop/uni/8sem/archlab/ex1/ex7_3/output_files"
+
+# List of folder names representing different CPUs
+cpu_folders = [
+    "blackscholes_outputs",
+    "fluidanimate_outputs",
+    "streamcluster_outputs",
+    "bodytrack_outputs",
+    "freqmine_outputs",
+    "swaptions_outputs",
+    "canneal_outputs",
+    "rtview_outputs"
+]
+
+# List to store IPC values for each CPU
+cpu_ipc_values = {}
+number_of_tests = 0
+
+# Iterate through each CPU folder
+for cpu_folder in cpu_folders:
+    cpu_type = cpu_folder.split("_")[0].capitalize()
+    ipc_values_test = []
+    # Iterate through each output.txt file in the CPU folder
+    for j in range(10):  
+        #if (j==2 or j==5 or j==8 or j==11):
+        	#ipc = 0
+        	#ipc_values_test.append(ipc)
+        	#continue
+        output_file_path = os.path.join(output_folder_path, cpu_folder, f"output_{j}.txt")
+        number_of_tests+=1
+        ipc = extract_ipc(output_file_path)
+        ipc_values_test.append(ipc)
+    cpu_ipc_values[cpu_type] = ipc_values_test
+
+# Calculate geometric mean for each test
+geometric_means = []
+for i in range(10):
+    ipc_values_at_index = [cpu_ipc_values[cpu_type][i] for cpu_type in cpu_ipc_values]
+    geometric_mean = np.prod(ipc_values_at_index) ** (1/len(ipc_values_at_index))
+    geometric_means.append(geometric_mean)
+
+# Plotting geometric means
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, 11), geometric_means, marker='o', color='blue')
+plt.title('Geometric Mean of IPC Values of all benchmarks for Different Tests')
+plt.xlabel('Test')
+plt.ylabel('Geometric Mean of IPC')
+plt.xticks(range(1, 11))
+plt.grid(True)
+plt.show()
+
+# Plotting
+plt.figure(figsize=(10, 6))
+for cpu_type, ipc_values in cpu_ipc_values.items():
+    max_ipc_value = max(ipc_values)
+    max_test_index = ipc_values.index(max_ipc_value) + 1
+    plt.plot(range(1, 11), ipc_values, label=cpu_type)
+    plt.scatter(max_test_index, max_ipc_value, color='red', marker='o')  # Marking max IPC
+    
+
+plt.title('IPC Values for Different CPUs')
+plt.xlabel('Test')
+plt.ylabel('IPC')
+plt.xticks(range(1, 11))
+plt.legend()
+plt.grid(True)
+plt.show()
+
